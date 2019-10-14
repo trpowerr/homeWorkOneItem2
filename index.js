5a32dc0ee683b86b6299e5708663c342a64f65cf
trpowerr <48128809+trpowerr@users.noreply.github.com>
2019-10-14 17:46:33 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict'
 var numbersAr = [25000, 5000, 40000, 40000];
 var winner = 0;
-for ( let value of numbersAr) {
+for ( var value of numbersAr) {
     if (winner < value) {
         winner = value;
     }
@@ -12,5 +12,5 @@ numbersAr.sort(function(a,b){
   return b - a;
 });
 
-let averageMark = Math.trunc((numbersAr[0] + numbersAr[1] + numbersAr[2]) / 3);
+var averageMark = Math.trunc((numbersAr[0] + numbersAr[1] + numbersAr[2]) / 3);
 console.log('Средний балл(ТОП-3): ' + averageMark);
