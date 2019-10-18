a05b47e4bcf2d8f09163803b754b93d93d31abc7
trpowerr <48128809+trpowerr@users.noreply.github.com>
2019-10-18 14:11:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 'use strict'
-var numbersAr = [25000, 5000, 40000, 40000];
+var numbersAr = [25000, 5000,40000, 40000];
 var winner = 0;
-for ( var value of numbersAr) {
-    if (winner < value) {
-        winner = value;
-    }
-}
-console.log('Лучший балл: ' + winner);
+// for ( var value of numbersAr) {
+//     if (winner < value) {
+//         winner = value;
+//     }
+// }
+
+numbersAr.forEach(function(value){
+  if (winner < value) {
+    winner = value;
+  }  
+});
+
+console.log(winner);
+
 
 numbersAr.sort(function(a,b){ 
   return b - a;
 });
 
-var averageMark = Math.trunc((numbersAr[0] + numbersAr[1] + numbersAr[2]) / 3);
-console.log('Средний балл(ТОП-3): ' + averageMark);
+console.log('Средний балл(ТОП-3): ' + (numbersAr[0] + numbersAr[1] + numbersAr[2]) / 3);
+
+// var averageMark = Math.trunc((numbersAr[0] + numbersAr[1] + numbersAr[2]) / 3);
+// console.log('Средний балл(ТОП-3): ' + averageMark);
